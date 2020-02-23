e62e253a7400f1bb4b848ddf244728953f717018
InSecker <pierresecher98@gmail.com>
2020-02-23 12:35:42 +0100

    heroky deployment

--- scripts/mysql_hydrator.sh
+++ scripts/mysql_hydrator.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker exec -i mysql mysql -u root -proot -e 'CREATE DATABASE IF NOT EXISTS `paris-jo`'
-docker exec -i mysql mysql -u root -proot paris-jo < paris-jo.sql
+mysql -h "server-url" -u root -proot paris-jo < paris-jo.sql
