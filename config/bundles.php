fdfac957ccafc35b8ea372bd4b85a72dd53695f8
InSecker <pierresecher98@gmail.com>
2020-02-24 10:30:22 +0100

    remove web server from dev dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['all' => true],
 ];
